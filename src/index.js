d143022446eacd6c56a38dd66203af5ae581b286
Michael de Vera <mdevera@M-MacBook-Pro.local>
2018-11-11 13:41:16 -0600

    Changed location for main App files.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.scss';
-import App from './App';
+import './Containers/Styles/_Global.scss'
+import App from './Containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
